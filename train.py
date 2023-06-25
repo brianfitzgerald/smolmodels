160fec92f6569d481edee038280fc85b9efe05da
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2023-06-25 12:54:25 -0500

    use hf model for now

--- train.py
+++ train.py
@@ -1,49 +1,87 @@
-from dataclasses import dataclass
-import os
-from typing import List, Optional, Tuple, Union
-
-import torch
-import torch.utils.checkpoint
-from torch import Tensor, nn
-from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
-import math
 from tokenizers import AutoTokenizer
+from torch.utils.data import DataLoader
+import evaluate
+import torch
 
-from model import *
-from transformers import GenerationMixin, DataCollatorForLanguageModeling
+from transformers import (
+    GenerationMixin,
+    DataCollatorForLanguageModeling,
+    get_scheduler,
+    AdamW,
+    GPTNeoForCausalLM,
+)
 from datasets import load_dataset
+from utils import get_available_device
+
 
 dataset = load_dataset("roneneldan/TinyStories")
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
-config = GPTNeoConfig()
 
-collator = DataCollatorForLanguageModeling(
+data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer, mlm=True, mlm_probability=0.15
 )
-dataset = dataset.map(
+
+tokenized_datasets = dataset.map(
     lambda x: tokenizer(
         x["text"],
         truncation=True,
-        padding="max_length",
-        max_length=config.max_position_embeddings,
         return_tensors="pt",
     ),
     batched=True,
 )
-dataset = dataset.remove_columns(["text"])
+tokenized_datasets = dataset.remove_columns(["text"])
 
-gpt = GPTNeoForCausalLM(config)
+train_dataloader = DataLoader(
+    tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
+)
 
 
-prompt = "Once upon a time there was"
+model = GPTNeoForCausalLM()
+num_epochs = 3
+optimizer = AdamW(model.parameters(), lr=5e-5)
 
-input_ids = tokenizer.encode(prompt, return_tensors="pt")
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
 
-sampler_mixin = GenerationMixin()
+device = get_available_device()
+model.to(device)
 
-output = gpt.generate(input_ids, max_length=1000, num_beams=1)
+from tqdm.auto import tqdm
 
-output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+progress_bar = tqdm(range(num_training_steps))
+metric = evaluate.load("glue", "mrpc")
 
-print(output_text)
+
+model.train()
+
+for epoch in range(num_epochs):
+    model.train()
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+    model.eval()
+    for batch in eval_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+        metric.add_batch(predictions=predictions, references=batch["labels"])
