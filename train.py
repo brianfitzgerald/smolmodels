b091c6fe739d2e37a13ab9dc1373cae554683f70
Brian <cooldude242@gmail.com>
2023-07-03 01:25:57 +0000

    refactoring

--- train.py
+++ train.py
@@ -1,9 +1,15 @@
+from enum import IntEnum
+from pprint import pprint
 from torch.utils.data import DataLoader
 import evaluate
 import torch
+from dataclasses import dataclass
+from evaluate import load
+import torch.nn.functional as F
+import os
 
 from transformers import (
-    DataCollatorWithPadding,
+    DataCollatorForLanguageModeling,
     GPTNeoConfig,
     get_scheduler,
     AutoTokenizer,
@@ -11,29 +17,99 @@ from transformers import (
     GPTNeoForCausalLM,
 )
 from datasets import load_dataset
-from utils import get_available_device
+from utils import *
+from tqdm.auto import tqdm
+import wandb
 
-# Script for training the base model
 
-dataset = load_dataset("roneneldan/TinyStories")
+class Task(IntEnum):
+    TINY_STORIES = 1
+    STATE_CHANGES = 2
 
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
-tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+os.environ["LD_LIBRARY_PATH"] = "/usr/lib/x86_64-linux-gnu/"
 
-tokenized_datasets = dataset.map(
-    lambda x: tokenizer(x["text"]),
+
+@dataclass
+class TrainingArgs:
+    task = Task.STATE_CHANGES
+    use_wandb = False
+    model_name = "smolmodels-finetune-33m-crd-instruct"
+
+
+if TrainingArgs.use_wandb:
+    wandb.login()
+    wandb_run = wandb.init(project=TrainingArgs.model_name)
+
+
+def calculate_bpc(model, evaluation_data):
+    total_loss = 0.0
+    total_characters = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        for input_seq, target_seq in evaluation_data:
+            input_seq = torch.tensor(input_seq).unsqueeze(0)
+            target_seq = torch.tensor(target_seq).unsqueeze(0)
+
+            output_seq = model(input_seq)
+            output_seq = output_seq.squeeze(0)
+
+            loss = F.cross_entropy(output_seq, target_seq)
+            total_loss += loss.item()
+            total_characters += target_seq.size(1)
+
+    average_loss = total_loss / total_characters
+    bpc = average_loss / torch.log(torch.tensor(2.0))
+
+    return bpc.item()
+
+
+tokenize_fn = None
+
+if TrainingArgs.task == Task.TINY_STORIES:
+    dataset = load_dataset("roneneldan/TinyStories")
+
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+    tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+    tokenize_fn = lambda x: tokenizer(x["text"])
+
+elif TrainingArgs.task == Task.STATE_CHANGES:
+    dataset = load_dataset("Fraser/python-state-changes")
+    tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-base")
+
+    def tokenize_state_changes(batch: dict):
+        concatted_batch = []
+        for i in range(len(batch["start"])):
+            concatted = (
+                batch["start"][i]
+                + tokenizer.sep_token
+                + batch["code"][i]
+                + tokenizer.sep_token
+                + batch["end"][i]
+                + tokenizer.sep_token
+            )
+            concatted_batch.append(concatted)
+
+        tokenized = tokenizer(
+            concatted_batch, padding="max_length", truncation=True, return_tensors="pt"
+        )
+        return tokenized
+
+    tokenize_fn = tokenize_state_changes
+
+tokenized_dataset = dataset.map(
+    tokenize_fn,
     batched=True,
 )
-tokenized_datasets.set_format("torch")
+tokenized_dataset = tokenized_dataset.remove_columns(["start", "code", "end"])
+tokenized_dataset.set_format("torch")
+tokenized_dataset = tokenized_dataset["train"].train_test_split(test_size=0.1)
 
-train_dataloader = DataLoader(
-    tokenized_datasets["train"], batch_size=8, collate_fn=data_collator
-)
-eval_dataloader = DataLoader(
-    tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
-)
+train_dataloader = DataLoader(tokenized_dataset["train"], batch_size=8)
+eval_dataloader = DataLoader(tokenized_dataset["test"], batch_size=8)
 
 config = GPTNeoConfig(
     hidden_size=768,
@@ -49,9 +125,12 @@ config = GPTNeoConfig(
 )
 
 model = GPTNeoForCausalLM(config)
-num_epochs = 3
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
+if TrainingArgs.use_wandb:
+    wandb.watch(model)
+
+num_epochs = 3
 num_training_steps = num_epochs * len(train_dataloader)
 lr_scheduler = get_scheduler(
     "linear",
@@ -63,21 +142,21 @@ lr_scheduler = get_scheduler(
 device = get_available_device()
 model.to(device)
 
-from tqdm.auto import tqdm
 
 progress_bar = tqdm(range(num_training_steps))
-metric = evaluate.load("glue", "mrpc")
 
 model.train()
 
 for epoch in range(num_epochs):
     print("epoch", epoch, train_dataloader)
-    for batch in train_dataloader:
+    for j, batch in enumerate(train_dataloader):
+        print(batch)
         batch = {k: v.to(device) for k, v in batch.items()}
-        print("batch", batch)
         outputs = model(**batch)
         loss = outputs.loss
         loss.backward()
+
+        wandb.log({"loss": loss.item()})
 
         optimizer.step()
         lr_scheduler.step()
@@ -86,10 +165,31 @@ for epoch in range(num_epochs):
 
     model.eval()
     for batch in eval_dataloader:
+        print(batch)
         batch = {k: v.to(device) for k, v in batch.items()}
         with torch.no_grad():
             outputs = model(**batch)
 
         logits = outputs.logits
+        input_ids = batch["input_ids"]
         predictions = torch.argmax(logits, dim=-1)
-        metric.add_batch(predictions=predictions, references=batch["labels"])
+
+        wandb.log({"loss": loss.item()})
+
+        prompt, generated_text = get_text_sample(logits, input_ids, tokenizer)
+        perplexity_score = get_perplexity(logits, input_ids)
+        log_dict = {
+            "prompt": prompt,
+            "generated_text": generated_text,
+            "perplexity_score": perplexity_score,
+        }
+        pprint(log_dict, indent=2)
+        if TrainingArgs.use_wandb:
+            wandb.log(log_dict)
+
+    if j % TrainingArgs.save_interval == 0:
+        save_file_path = os.path.join("checkpoints", f"model_epoch_{epoch}_batch_{j}")
+        if TrainingArgs.use_peft:
+            model.save_pretrained(save_file_path, safe_serialization=True)
+        if TrainingArgs.push_model:
+            model.push_to_hub(TrainingArgs.model_name)
--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+from dataclasses import dataclass
+from enum import IntEnum
 import torch
 import os
 import platform
 import requests
+import torch.nn.functional as F
+from transformers import AutoTokenizer
+
 
 def get_available_device():
     device = torch.device("cpu")
@@ -17,7 +22,9 @@ def should_use_wandb():
         return False
     return os.environ.get("USER") == "ubuntu" and platform.system().lower() == "linux"
 
+
 # https://github.com/teknium1/GPTeacher/blob/main/Roleplay/roleplay-simple-deduped-roleplay-instruct.json
+
 
 def download_if_not_present(file_path: str, url: str):
     # Define the file path and URL
@@ -29,8 +36,67 @@ def download_if_not_present(file_path: str, url: str):
         # Download the file
         response = requests.get(url)
         if response.status_code == 200:
-            with open(file_path, 'wb') as file:
+            with open(file_path, "wb") as file:
                 file.write(response.content)
             print("File downloaded successfully.")
         else:
             print("Failed to download the file.")
+
+
+class DatasetChoice(IntEnum):
+    CRD = 1
+    ROLEPLAY_INSTRUCT = 2
+
+
+@dataclass
+class TrainingArgs:
+    ds_choice = DatasetChoice.CRD
+    num_epochs = 24
+    batch_size = 8
+    save_interval = 2
+    eval_interval = 1
+    use_wandb = True
+    push_model = False
+    model_name = "smolmodels-finetune-33m-dialogue"
+    use_peft = False
+
+
+def get_perplexity(logits: torch.Tensor, input_ids: torch.Tensor):
+    loss = F.cross_entropy(logits.view(-1, logits.size(-1)), input_ids.view(-1))
+    perp = torch.exp(loss)
+    return perp.item()
+
+
+def get_text_sample(
+    logits: torch.Tensor, input_ids: torch.Tensor, tokenizer: AutoTokenizer
+):
+    decoded_input = tokenizer.batch_decode(input_ids)
+    sample_batchsize, seq_length, _ = logits.size()
+    decoded_texts = []
+
+    for i in range(sample_batchsize):
+        decoded_tokens = []
+        for j in range(seq_length):
+            token_id = torch.argmax(logits[i, j]).item()
+            token = tokenizer.decode([token_id])
+            decoded_tokens.append(token)
+
+        decoded_text = tokenizer.convert_tokens_to_string(decoded_tokens)
+        decoded_texts.append(decoded_text)
+
+    return decoded_input, decoded_texts
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
