e3e2829554cf2537e85c3a400bb30abbb5d05bff
Your Name <you@example.com>
2023-11-19 03:33:38 +0000

    working

--- chat.py
+++ chat.py
@@ -3,61 +3,68 @@ from tokenizer import Tokenizer
 import re
 
 
-def model_conversation_input(
-    user_input: str,
-    message_history: List[Dict],
-    prompt_enhancer_mode: bool = False,
-):
+def dalle_conversation(user_input: str):
     """
     Example messages come from the DALL-E 3 technical report:
     https://cdn.openai.com/papers/dall-e-3.pdf.
     """
 
-    if prompt_enhancer_mode:
-        user_conversation: List[Dict] = []
-        system_message = {
-            "role": "system",
-            "content": """You are part of a team of bots that creates images. You work with an assistant bot that will draw anything you say in square brackets. For example, outputting "a beautiful morning in the woods with the sun peaking through the trees" will trigger your partner bot to output an image of a forest morning, as described. You will be prompted by people looking to create detailed, amazing images. The way to accomplish this is to take their short prompts and make them extremely detailed and descriptive.
+    system_message = {
+        "role": "system",
+        "content": """You are part of a team of bots that creates images. You work with an assistant bot that will draw anything you say in square brackets. For example, outputting "a beautiful morning in the woods with the sun peaking through the trees" will trigger your partner bot to output an image of a forest morning, as described. You will be prompted by people looking to create detailed, amazing images. The way to accomplish this is to take their short prompts and make them extremely detailed and descriptive.
 
-        There are a few rules to follow:
+    There are a few rules to follow:
 
-        - You will only ever output a single image description per user request.
-        - Sometimes the user will request that you modify previous captions. In this case, you should refer to your previous conversations with the user and make the modifications requested.
-        - When modifications are requested, you should not simply make the description longer. You should refactor the entire description to integrate the suggestions.
-        - Other times the user will not want modifications, but instead want a new image. In this case, you should ignore your previous conversation with the user."
-        - Image descriptions must be between 15-80 words. Extra words will be ignored.
-        """,
-        }
+    - You will only ever output a single image description per user request.
+    - Sometimes the user will request that you modify previous captions. In this case, you should refer to your previous conversations with the user and make the modifications requested.
+    - When modifications are requested, you should not simply make the description longer. You should refactor the entire description to integrate the suggestions.
+    - Other times the user will not want modifications, but instead want a new image. In this case, you should ignore your previous conversation with the user."
+    - Image descriptions must be between 15-80 words. Extra words will be ignored.
+    """,
+    }
 
-        dalle_base_conversation = [
-            {
-                "role": "user",
-                "content": "Create an imaginative image descriptive caption or modify an earlier caption for the user input : 'make the light red'",
-            },
-            {
-                "role": "assistant",
-                "content": "a pale figure with long white hair stands in the center of a dark forest, holding a sword high above his head. the blade glows with a red light, casting a warm glow on the trees and bushes surrounding him.",
-            },
-            {
-                "role": "user",
-                "content": "Create an imaginative image descriptive caption or modify an earlier caption for the user input : 'draw a frog playing dominoes'",
-            },
-            {
-                "role": "assistant",
-                "content": "a frog sits on a worn table playing a game of dominoes with an elderly raccoon. the table is covered in a green cloth, and the frog is wearing a jacket and a pair of jeans. The scene is set in a forest, with a large tree in the background.",
-            },
-        ]
+    dalle_base_conversation = [
+        {
+            "role": "user",
+            "content": "Create an imaginative image descriptive caption or modify an earlier caption for the user input : 'make the light red'",
+        },
+        {
+            "role": "assistant",
+            "content": "a pale figure with long white hair stands in the center of a dark forest, holding a sword high above his head. the blade glows with a red light, casting a warm glow on the trees and bushes surrounding him.",
+        },
+        {
+            "role": "user",
+            "content": "Create an imaginative image descriptive caption or modify an earlier caption for the user input : 'draw a frog playing dominoes'",
+        },
+        {
+            "role": "assistant",
+            "content": "a frog sits on a worn table playing a game of dominoes with an elderly raccoon. the table is covered in a green cloth, and the frog is wearing a jacket and a pair of jeans. The scene is set in a forest, with a large tree in the background.",
+        },
+    ]
 
-        formatted_new_prompt = f"Create an imaginative image descriptive caption or modify an earlier caption for the user input : '{user_input}'"
-        user_conversation.append({"role": "user", "content": formatted_new_prompt})
-        all_messages: List[Dict] = [*dalle_base_conversation, *user_conversation]
-    else:
-        system_message = {
-            "role": "system",
-            "content": "A chat between a human and an assistant named Steve. The assistant gives helpful, detailed, and polite answers to the user's questions.",
-        }
+    formatted_new_prompt = f"Create an imaginative image descriptive caption or modify an earlier caption for the user input : '{user_input}'"
+    all_messages: List[Dict] = [
+        system_message,
+        *dalle_base_conversation,
+        {"role": "user", "content": formatted_new_prompt},
+    ]
 
-        all_messages: List[Dict] = [system_message, *message_history]
+    return all_messages
+
+
+def model_conversation_input(
+    message_history: List[Dict],
+):
+    """
+    Format the user input and message history into ChatML.
+    """
+
+    system_message = {
+        "role": "system",
+        "content": "A chat between a human and an assistant named Steve. The assistant gives helpful, detailed, and polite answers to the user's questions.",
+    }
+
+    all_messages: List[Dict] = [system_message, *message_history]
 
     full_conversation_formatted = [
         f"<|im_start|>{message['role']}\n{message['content']}<|im_end|>"
--- generate_vllm.py
+++ generate_vllm.py
@@ -4,7 +4,6 @@ from awq.models.base import BaseAWQForCausalLM
 from transformers import AutoTokenizer
 import fire
 from vllm import LLM, SamplingParams
-from chat import model_conversation_input, extract_text_from_generated_message
 from pathlib import Path
 import time
 import torch.nn as nn
@@ -15,7 +14,7 @@ import json
 import torch.nn as nn
 from safetensors.torch import save_file
 from transformers.modeling_utils import shard_checkpoint
-from diffusers import DiffusionPipeline, LCMScheduler # type: ignore
+from diffusers import DiffusionPipeline, LCMScheduler  # type: ignore
 import torch
 from PIL import Image
 
@@ -23,31 +22,33 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+from chat import dalle_conversation, extract_text_from_generated_message
 
 
 # https://github.com/vllm-project/vllm/pull/1235/files
 
-def save_quantized(model: BaseAWQForCausalLM, save_dir: str, shard_size="10GB"):
 
+def save_quantized(model: BaseAWQForCausalLM, save_dir: str, shard_size="10GB"):
     # Save model
     class EmptyModule(nn.Module):
-        def __init__(self): super(EmptyModule, self).__init__()
-        def forward(self, x): return x
+        def __init__(self):
+            super(EmptyModule, self).__init__()
+
+        def forward(self, x):
+            return x
 
     # Save model files with empty state dict
     model.model.save_pretrained(save_dir, state_dict=EmptyModule().state_dict())
 
     # model_name has no extension, add it when saving state_dict
-    model_name = 'model.safetensors'
+    model_name = "model.safetensors"
 
     # Remove empty state dict
-    os.remove(f'{save_dir}/{model_name}')
+    os.remove(f"{save_dir}/{model_name}")
 
     # shard checkpoint into chunks (10GB default)
     shards, index = shard_checkpoint(
-        model.model.state_dict(), 
-        max_shard_size=shard_size, 
-        weights_name=model_name
+        model.model.state_dict(), max_shard_size=shard_size, weights_name=model_name
     )
 
     for shard_file, shard in shards.items():
@@ -57,18 +58,18 @@ def save_quantized(model: BaseAWQForCausalLM, save_dir: str, shard_size="10GB"):
 
     # save shard index
     if index is not None:
-        with open(f'{save_dir}/{model_name}.index.json', 'w+') as file:
+        with open(f"{save_dir}/{model_name}.index.json", "w+") as file:
             file.write(json.dumps(index, indent=4))
 
     # Save config
-    with open(f'{save_dir}/quant_config.json', 'w+') as file:
+    with open(f"{save_dir}/quant_config.json", "w+") as file:
         file.write(json.dumps(model.quant_config.to_dict(), indent=4))
+
 
 def main(
     # force re-quantization even if the quantized model exists
     force_quantize: bool = False,
 ):
-
     # Load token
 
     load_dotenv(".env")
@@ -100,7 +101,9 @@ def main(
         vllm_model = AutoAWQForCausalLM.from_pretrained(
             language_model_id, **{"low_cpu_mem_usage": True}
         )
-        tokenizer = AutoTokenizer.from_pretrained(language_model_id, trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(
+            language_model_id, trust_remote_code=True
+        )
         # make sure to change the os.remove line
         vllm_model.quantize(tokenizer, quant_config=quant_config)
         save_quantized(vllm_model, quantized_model_path_str)
@@ -112,14 +115,20 @@ def main(
     tokenizer = AutoTokenizer.from_pretrained(language_model_id, trust_remote_code=True)
     sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=128)
     vllm_model = LLM(quantized_model_path_str, dtype="auto", quantization="awq")
-    
-    print("Loading diffusion model...")
-    diffusion_pipeline = DiffusionPipeline.from_pretrained(diffusion_model_id, variant="fp16")
 
+    print("Loading diffusion model...")
+    diffusion_pipeline = DiffusionPipeline.from_pretrained(
+        diffusion_model_id, variant="fp16"
+    )
+
+    print("Loading LCM lora...")
     diffusion_pipeline.load_lora_weights(diffusion_lora_id)
-    diffusion_pipeline.scheduler = LCMScheduler.from_config(diffusion_pipeline.scheduler.config)
+    diffusion_pipeline.scheduler = LCMScheduler.from_config(
+        diffusion_pipeline.scheduler.config
+    )
     diffusion_pipeline.to(device="cuda", dtype=torch.float16)
 
+    print("Starting bot...")
     intents = discord.Intents.default()
     intents.message_content = True
     intents.members = True
@@ -139,30 +148,33 @@ def main(
 
     @bot.listen()
     async def on_message(message: discord.Message):
-        print('get message', message.author.id, message.author.name, message.channel.id)
         assert bot.user
         if message.author.id == bot.user.id:
             return
         if message.channel.id not in allowed_channels:
             return
-        model_input = model_conversation_input(message.content, [], prompt_enhancer_mode=False)
+        messages = dalle_conversation(message.content)
+        templated_messages: str = tokenizer.apply_chat_template(messages, tokenize=False)  # type: ignore
         t0 = time.perf_counter()
-        outputs = vllm_model.generate(model_input, sampling_params)
+        outputs = vllm_model.generate(
+            templated_messages, sampling_params=sampling_params
+        )
         outputs_text = outputs[0].outputs[0].text
         outputs_text = extract_text_from_generated_message(outputs_text)
         generated_image: Image.Image = diffusion_pipeline(
-            prompt=outputs_text,
-            num_inference_steps=4,
-            guidance_scale=1
+            prompt=outputs_text, num_inference_steps=4, guidance_scale=1
         ).images[0] # type: ignore
         with BytesIO() as image_binary:
-            generated_image.save(image_binary, 'PNG')
+            generated_image.save(image_binary, "PNG")
             image_binary.seek(0)
-            await message.channel.send(outputs_text, file=discord.File(fp=image_binary, filename='image.png'))
+            await message.channel.send(
+                outputs_text, file=discord.File(fp=image_binary, filename="image.png")
+            )
         print(outputs_text)
         print(f"Time taken: {time.perf_counter() - t0:.2f}s")
 
     bot.run(discord_token)
+
 
 if __name__ == "__main__":
     fire.Fire(main)
