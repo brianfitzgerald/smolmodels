2da6a7f09ad926474accf9b68a0d11076c0663fd
brianf <brian@stability.ai>
2024-02-10 02:34:09 +0000

    logging stuff

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from tabulate import tabulate
 import pandas as pd
 from transformers.models.t5.tokenization_t5 import T5Tokenizer
 from enum import Enum
+from pathlib import Path
+import shutil
 
 from model.t5 import T5FineTuner
 from model.llama import LlamaFineTuner
@@ -59,6 +61,10 @@ class LogPredictionSamplesCallback(pl.Callback):
         self.max_new_tokens = max_new_tokens
 
         # TODO clear existing log files
+        self.log_dir = Path("logs")
+        self.log_dir.mkdir(exist_ok=True)
+        shutil.rmtree(self.log_dir)
+        self.log_dir.mkdir(exist_ok=True)
 
     def on_validation_batch_end(
         self, trainer, pl_module, outputs, batch, batch_idx: int
@@ -68,6 +74,8 @@ class LogPredictionSamplesCallback(pl.Callback):
     def log_prediction_samples(
         self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
     ):
+        if batch_idx > 0:
+            return
         input_ids = batch["input_ids"]
         labels = batch["input_ids"]
         out = pl_module.model.generate(
@@ -87,23 +95,26 @@ class LogPredictionSamplesCallback(pl.Callback):
             )
             table_columns.append(decoded)
 
-        run_name = self.wandb_logger.experiment.name if self.wandb_logger else "latest"
+        run_name = "latest"
+        if self.wandb_logger:
+            run_name = self.wandb_logger.experiment.name
+            self.wandb_logger.log_table("Validation Samples", columns, table_columns)
 
         rows = [list(row) for row in zip(*table_columns)]
         rows_df = pd.DataFrame(rows, columns=columns)
         rows_df.to_csv(
-            f"run_{run_name}_samples.csv", mode="a", header=False, index=False
+            self.log_dir / f"{run_name}_samples.csv", mode="a", header=False, index=False
         )
 
-        if batch_idx == 0:
-            new_rows = tabulate(
-                rows,
-                headers=columns,
-                tablefmt="simple_outline",
-            )
-            with open(f"run_{run_name}_samples.txt", "a") as f:
-                f.write(new_rows)
-                f.write("\n")
+        new_rows = tabulate(
+            rows,
+            headers=columns,
+            tablefmt="simple_outline",
+            maxcolwidths=[10, 10, 100, 100, 100],
+        )
+        with open(self.log_dir / f"{run_name}_samples.txt", "a") as f:
+            f.write(new_rows)
+            f.write("\n")
 
 
 class ModelChoice(Enum):
