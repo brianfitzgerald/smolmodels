9bad82a404695540faf67d066faad25c66d7efba
brianf <brian@stability.ai>
2024-02-13 01:31:21 +0000

    upload script

--- eval.py
+++ eval.py
@@ -1,6 +1,9 @@
 print("Loading dependencies...")
 from transformers.models.t5.modeling_t5 import T5ForConditionalGeneration
 from transformers.models.t5.tokenization_t5 import T5Tokenizer
+from huggingface_hub import login
+import os
+from dotenv import load_dotenv
 
 from model.utils import TASK_PREFIX
 import pandas as pd
@@ -8,17 +11,29 @@ from fire import Fire
 
 
 def main(
-    checkpoint_path: str = "",
+    checkpoint_dir: str = "checkpoints/best_model-v1.ckpt.dir",
     batch_size: int = 8,
+    upload_to_hf: bool = False,
 ):
 
     print("Loading model...")
-    tokenizer = T5Tokenizer.from_pretrained(checkpoint_path)
+    tokenizer = T5Tokenizer.from_pretrained(checkpoint_dir)
     model: T5ForConditionalGeneration = T5ForConditionalGeneration.from_pretrained(
-        checkpoint_path
+        checkpoint_dir
     )
 
     drawbench_df: pd.DataFrame = pd.read_csv("data/drawbench.csv")
+
+    if upload_to_hf:
+        file_dir = os.path.dirname(os.path.abspath(__file__))
+        load_dotenv(os.path.join(file_dir, ".env"))
+        token = os.getenv("HF_TOKEN")
+        print(f"Logging in with token: {token}")
+        login(token=token, add_to_git_credential=True)
+
+        print("Uploading to HuggingFace...")
+        model.push_to_hub("superprompt-v1")
+        return
 
     for i in range(0, len(drawbench_df), batch_size):
 
@@ -29,13 +44,15 @@ def main(
         inputs = tokenizer(prompts_with_prefix, return_tensors="pt", padding=True)
 
         output_sequences = model.generate(
-            input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs["attention_mask"],
+            max_length=256,
+            num_return_sequences=1,
         )
 
         out = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
-        print(out)
-
-    print(tokenizer.batch_decode(output_sequences, skip_special_tokens=True))
+        for prompt, output in zip(chunk["Prompt"], out):
+            print(f"Prompt: {prompt}\nOutput: {output}\n\n")
 
 
 if __name__ == "__main__":
