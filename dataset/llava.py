eb0ce8c4714d6b780b5d697bfe196db45041286a
brianf <brian@stability.ai>
2024-02-17 05:33:57 +0000

    wip

--- dataset/llava.py
+++ dataset/llava.py
@@ -8,39 +8,40 @@ import lightning.pytorch as pl
 
 from model.utils import TASK_PREFIX
 
+
 class LlavaDataModule(pl.LightningDataModule):
     def __init__(
         self,
         batch_size: int,
+        tokenizer: PreTrainedTokenizer,
+        max_token_length: int,
     ):
         super().__init__()
+
         self.train_dataset = None
         self.val_dataset = None
+        self.batch_size = batch_size
+        self.tokenizer = tokenizer
+        self.max_token_length = max_token_length
 
     def setup(self, stage: Optional[str] = None):
         print(f"Loading dataset for stage {stage}")
 
-        dataset = load_dataset("liuhaotian/LLaVA-CC3M-Pretrain-595K")["train"].train_test_split(test_size=0.01) # type: ignore
+        dataset = load_dataset("liuhaotian/LLaVA-CC3M-Pretrain-595K")["train"].train_test_split(test_size=0.01)  # type: ignore
         self.train_dataset = dataset["train"]
         self.val_dataset = dataset["test"]
 
         cache_dir = "dataset_cache_llava"
         Path(cache_dir).mkdir(exist_ok=True)
-        
+
         # Set format for PyTorch
         self.train_dataset.set_format(type="torch")
         self.val_dataset.set_format(type="torch")
 
     def prepare_sample(self, examples: dict):
-        processed_batch = {}
-
-        inputs = [
-            TASK_PREFIX + doc
-            for doc in examples["Prompt"]
-        ]
 
         inputs_tokenized = self.tokenizer(
-            inputs,
+            examples["conversations"],
             max_length=self.max_token_length,
             truncation=True,
             padding="max_length",
@@ -61,7 +62,6 @@ class LlavaDataModule(pl.LightningDataModule):
             "decoder_attention_mask": labels_tokenized["attention_mask"],
             "labels": labels_tokenized["input_ids"],
         }
-        return processed_batch
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=35)  # type: ignore
