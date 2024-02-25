5f228a853cb87fb433ec366bdb84caa0aee6c7b2
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2024-02-25 15:55:39 -0600

    full loop

---
+++ synthetic_data/conversion.py
@@ -0,0 +1,66 @@
+import re
+from typing import Dict, List
+
+GLAIVE_ROLES = ["USER", "ASSISTANT", "FUNCTION RESPONSE"]
+GLAIVE_TO_SHAREGPT_ROLE = {
+    "SYSTEM": "system",
+    "USER": "human",
+    "ASSISTANT": "gpt",
+    "FUNCTION RESPONSE": "tool",
+}
+
+
+def chatml_to_conversation(row: Dict[str, str]) -> List[Dict[str, str]]:
+    """
+    Convert a string in ChatML format to a list of conversation steps.
+    Taken from https://github.com/lilacai/lilac/blob/main/notebooks/GlaiveToShareGPT.ipynb
+    """
+    split_re = re.compile(r"({}): ".format("|".join(GLAIVE_ROLES)))
+
+    system_prompt = row.get("system")
+    # Remove "SYSTEM: " from the beginning of the prompt.
+    if system_prompt:
+        system_prompt = system_prompt.removeprefix("SYSTEM: ")
+
+    chat = row["chat"]
+    # Split chat by split_res, and remove empty strings.
+    chats = [s.strip() for s in split_re.split(chat) if s]
+
+    # results look like:
+    # ['USER', 'Can you book a flight for me from New York to London?', 'ASSISTANT', '...']
+    # We now want it to be a dictionary of {'from': 'user', 'value': 'Can you book a flight...'}
+    chats = [
+        {"from": GLAIVE_TO_SHAREGPT_ROLE[role], "value": value}
+        for role, value in zip(chats[::2], chats[1::2])
+    ]
+
+    if system_prompt:
+        chats = [
+            {"from": GLAIVE_TO_SHAREGPT_ROLE["SYSTEM"], "value": system_prompt}
+        ] + chats
+
+    return chats
+
+
+def merge_consecutive_messages(messages):
+    """
+    Merge messages by the same sender that are consecutive
+    """
+
+    merged_messages = []
+    current_from = None
+    current_message = ""
+
+    for msg in messages:
+        if current_from == msg['from']:
+            current_message += msg['value']
+        else:
+            if current_from is not None:
+                merged_messages.append({'from': current_from, 'value': current_message})
+            current_from = msg['from']
+            current_message = msg['value']
+
+    if current_from is not None:
+        merged_messages.append({'from': current_from, 'value': current_message})
+
+    return merged_messages
