64a1ab68d3bb90958130ee9d8f83cc0f18e1d76b
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2024-02-26 22:56:46 -0600

    wip

---
+++ synthetic_data/utils.py
@@ -0,0 +1,30 @@
+import re
+from typing import Dict, List
+
+from tabulate import tabulate
+
+
+def clean_message(message: str) -> str:
+    """
+    Clean up spaces, tabs, and newlines in a message, so the JSON is formatted nicely.
+    """
+    message = message.strip()
+    message = re.sub(r"\n+|\t+", "", message)
+    message = re.sub(r"\s+", " ", message)
+    message = message.replace("<|endoftext|>", "")
+    return message
+
+
+def print_conversations_table(results: List[Dict]):
+    new_dataset_row_elements = [
+        (clean_message(row["system"]), row["question"], row["chosen"], row["rejected"])
+        for row in results
+    ]
+    breakpoint()
+    print(
+        tabulate(
+            new_dataset_row_elements,
+            headers=["System", "Question", "Chosen", "Rejected"],
+            maxcolwidths=[50] * 4,
+        )
+    )
