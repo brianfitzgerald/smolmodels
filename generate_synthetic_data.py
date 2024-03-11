7c4bc2cf36832d604c5da5cab6ed0e4bcedfaf8a
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2024-03-10 22:22:46 -0500

    wip

--- generate_synthetic_data.py
+++ generate_synthetic_data.py
@@ -1,17 +1,13 @@
 import asyncio
 import os
-import random
 import traceback
-from dataclasses import dataclass
-from typing import Dict, List, Optional, cast
+from typing import Dict, List, cast
 
 import fire
-import pandas as pd
 from datasets import Dataset, load_dataset
 from datasets.data_files import EmptyDatasetError
 from dotenv import dotenv_values
 from huggingface_hub import login
-from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
 from synthetic_data.tasks import (
     PromptUpsample,
     SyntheticDataTask,
@@ -19,10 +15,7 @@ from synthetic_data.tasks import (
     Toolformer,
 )
 
-from synthetic_data.conversion import chatml_to_conversation
 from synthetic_data.generation import (
-    SHAREGPT_TO_OPENAI_ROLE,
-    Conversation,
     GenerationWrapper,
     GroqGenerationWrapper,
     OpenAIGenerationWrapper,
@@ -30,24 +23,12 @@ from synthetic_data.generation import (
     VLLMWrapper,
     upload_dataset,
 )
-from synthetic_data.prompts import (
-    TOOL_USE_CATEGORIES,
-    format_dalle_prompt_template,
-    get_tool_usage_prompt,
-    get_toolformer_dpo_negative_completion_prompt,
-    get_toolformer_prompt,
-)
 from synthetic_data.utils import (
     DatasetTaskFormat,
     GenerationSource,
     SeedDataFormat,
-    ToolFormerDPORow,
-    ToolFormerRow,
     assert_valid_python_code,
-    clean_message,
-    extract_tool_usage_dpo_row,
     extract_toolformer_dpo_row,
-    extract_toolformer_row,
     print_result_dicts,
 )
 
@@ -68,9 +49,8 @@ def main(
     # n batches
     upload_every: int = 10,
     batch_size: int = 16,
-    n_epochs: int = 10,
     restart: bool = False,
-    generate_pairs: bool = False,
+    generate_dpo_pairs: bool = False,
     generation_source: GenerationSource = GenerationSource.OPENROUTER,
     task_name: str = "toolformer_pairs",
     **kwargs,
@@ -86,7 +66,7 @@ def main(
 
     task = DATA_TASKS[task_name]()
 
-    if generate_pairs and task.dataset_task_format != DatasetTaskFormat.DPO:
+    if generate_dpo_pairs and task.dataset_task_format != DatasetTaskFormat.DPO:
         raise ValueError("generate_pairs is only supported for DPO tasks.")
 
     print("Logging into the Hub...")
@@ -125,247 +105,47 @@ def main(
         input_dataset = cast(
             Dataset, load_dataset("tsv", data_files=task.seed_data_location)
         )
+    elif task.seed_data_format == SeedDataFormat.SYNTHETIC and task.dpo_task_cache_dataset_name:
+        input_dataset = cast(
+            Dataset, load_dataset(task.dpo_task_cache_dataset_name, split="train")
+        )
 
     new_dataset_rows: List[Dict] = []
     print("Running...")
 
-    if generate_pairs:
-            # Generate negative pairs for toolformer
-            # task, definition, tool_call, call_result, agent_output
-            for batch_idx, batch in enumerate(
-                input_dataset.iter(batch_size=batch_size)
-            ):
-                full_conversations_batch = []
-                new_rows_batch = []
-                original_rows = []
-                # TODO chance of dropping out tool definition
-                for conversation in batch["conversations"]:  # type: ignore
-                    messages = [message["content"] for message in conversation]
-                    question, tool_call, call_result, agent_output = messages
-                    original_row = ToolFormerRow(
-                        question=question,
-                        call_result=call_result,
-                        tool_call=tool_call,
-                        agent_output=agent_output,
-                    )
-                    original_rows.append(original_row)
-                    conversation = get_toolformer_dpo_negative_completion_prompt(
-                        question
-                    )
-                    full_conversations_batch.append(conversation)
-
-                print(f"Generating {len(full_conversations_batch)} completions...")
-                completions = asyncio.run(
-                    model_wrapper.generate(full_conversations_batch)
-                )
-
-                for j, completion in enumerate(completions):
-                    try:
-                        row = extract_toolformer_dpo_row(completion, original_rows[j])
-
-                        assert_valid_python_code(row.tool_call_accepted)
-                        assert_valid_python_code(row.tool_call_rejected)
-
-                        row_dict = row.__dict__
-                        new_rows_batch.append(row_dict)
-                    except Exception as e:
-                        traceback.print_exc()
-                        continue
-                print_result_dicts(new_rows_batch)
-                new_dataset_rows.extend(new_rows_batch)
-                if batch_idx % upload_every == 0 and batch_idx > 0:
-                    upload_dataset(
-                        output_dataset, task.output_dataset_name, new_dataset_rows
-                    )
-
-        else:
-            raise ValueError(
-                "generate_dpo_negative_pairs is only supported for TOOLFORMER."
-            )
-    else:
-        for epoch_idx in range(n_epochs):
-
-            # Seed dataset, so generate the prompt and negative sample
-            prompt_conversations: List[Conversation] = []
-            if task:
-                seed_data = pd.read_csv(task.seed_data_location, on_bad_lines="skip")
-                num_batches = len(seed_data) // batch_size + 1
-
-                # Iterate through batches
-                for i in range(num_batches):
-                    start_idx = i * batch_size
-                    end_idx = min((i + 1) * batch_size, len(seed_data))
-                    seed_data_batch = seed_data.iloc[start_idx:end_idx]
-
-                    for _, seed_data_row in seed_data_batch.iterrows():
-                        prompt_conversations.append(
-                            get_tool_usage_prompt(
-                                seed_data_row["Category"],
-                                seed_data_row["Task"],
-                            )
-                        )
-            elif config.dataset_task == DatasetTask.TOOLFORMER:
-                # Iterate through batches
-                random_categories = random.sample(
-                    TOOL_USE_CATEGORIES * batch_size, batch_size
-                )
-                for category in random_categories:
-                    prompt_conversations.append(get_toolformer_prompt(category))
-
-            # Generate completions
-
-            try:
-                print(
-                    f"Generating {len(prompt_conversations)} completions for epoch {epoch_idx}..."
-                )
-                completions = asyncio.run(model_wrapper.generate(prompt_conversations))
-            except Exception as e:
-                print(f"Error generating completions: {e}")
-                continue
-
+    if generate_dpo_pairs:
+        # Generate negative pairs for toolformer
+        # task, definition, tool_call, call_result, agent_output
+        for batch_idx, batch in enumerate(
+            input_dataset.iter(batch_size=batch_size)
+        ):
+            full_conversations_batch = []
             new_rows_batch = []
-            for completion in completions:
+            original_rows = []
+
+            print(f"Generating {len(full_conversations_batch)} completions...")
+            completions = asyncio.run(
+                model_wrapper.generate(full_conversations_batch)
+            )
+
+            for j, completion in enumerate(completions):
                 try:
-                    if config.dataset_task == DatasetTask.TOOL_USAGE_DPO:
-                        row = extract_tool_usage_dpo_row(completion)
+                    row = extract_toolformer_dpo_row(completion, original_rows[j])
 
-                        assert_valid_python_code(row.definition)
-                        assert_valid_python_code(row.call_result)
-                        assert_valid_python_code(row.tool_call)
+                    assert_valid_python_code(row.tool_call_accepted)
+                    assert_valid_python_code(row.tool_call_rejected)
 
-                        new_rows_batch.append(
-                            {
-                                "tool": row.definition,
-                                "question": row.task,
-                                "call_result": row.call_result,
-                                "tool_call": row.tool_call,
-                                "agent_output": row.agent_output,
-                            }
-                        )
-                    elif config.dataset_task == DatasetTask.TOOLFORMER:
-                        row = extract_toolformer_row(completion)
-                        reformatted_conversation = [
-                            {"role": "user", "content": row.question},
-                            {"role": "tool", "content": row.tool_call},
-                            {"role": "tool", "content": row.call_result},
-                            {"role": "assistant", "content": row.agent_output},
-                        ]
-                        # TODO validate output
-                        new_rows_batch.append(
-                            {
-                                "conversations": reformatted_conversation,
-                            }
-                        )
+                    row_dict = row.__dict__
+                    new_rows_batch.append(row_dict)
                 except Exception as e:
                     traceback.print_exc()
                     continue
-
-            dict_of_steps = [
-                {
-                    f"step_{index}": value["content"]
-                    for index, value in enumerate(row["conversations"])
-                }
-                for row in new_rows_batch
-            ]
-            print_result_dicts(dict_of_steps)
+            print_result_dicts(new_rows_batch)
             new_dataset_rows.extend(new_rows_batch)
-
-            if epoch_idx % upload_every == 0 and epoch_idx > 0:
+            if batch_idx % upload_every == 0 and batch_idx > 0:
                 upload_dataset(
-                    output_dataset, config.output_dataset_name, new_dataset_rows
+                    output_dataset, task.output_dataset_name, new_dataset_rows
                 )
-
-            # Generate completions for existing prompts
-            else:
-                for i, batch in enumerate(input_dataset.iter(batch_size=batch_size)):
-                    new_rows_batch = []
-                    if config.dataset_task == DatasetTask.PROMPT_UPSAMPLE:
-                        prompts, categories_batch = batch["Prompt"], batch["Category"]  # type: ignore
-                        full_conversations_batch: List[Conversation] = [
-                            format_dalle_prompt_template(prompt) for prompt in prompts
-                        ]
-                        completions = asyncio.run(
-                            model_wrapper.generate(full_conversations_batch)
-                        )
-
-                        for category, original_prompt, output in zip(
-                            categories_batch, prompts, completions
-                        ):
-                            new_rows_batch.append(
-                                {
-                                    "Prompt": original_prompt,
-                                    "Category": category,
-                                    "Upsampled": output,
-                                }
-                            )
-
-                            print(
-                                f"Epoch: {epoch_idx} idx: {i} ({category}): {original_prompt} -> {output}"
-                            )
-                    elif config.dataset_task == DatasetTask.TOOL_USAGE_DPO:
-                        full_conversations_batch: List[
-                            List[ChatCompletionMessageParam]
-                        ] = []
-
-                        glaive_conversations = [chatml_to_conversation(chat, system) for chat, system in zip(batch["chat"], batch["system"])]  # type: ignore
-                        prompt_conversations: List[Conversation] = []
-                        for conversation in glaive_conversations:
-                            completion_conv = []
-                            for msg in conversation:
-                                if msg["from"] == "gpt":
-                                    break
-                                completion_conv.append(
-                                    {
-                                        "role": SHAREGPT_TO_OPENAI_ROLE[msg["from"]],
-                                        "content": msg["value"],
-                                    }
-                                )
-                            prompt_conversations.append(completion_conv)
-
-                        print(
-                            f"Generating {len(prompt_conversations)} completions for batch {i}..."
-                        )
-                        completions = asyncio.run(
-                            model_wrapper.generate(prompt_conversations)
-                        )
-
-                        for completion, glaive_conversation in zip(
-                            completions, glaive_conversations
-                        ):
-
-                            system_msg, user_msg, accepted_msg, rejected_msg = (
-                                "",
-                                "",
-                                "",
-                                "",
-                            )
-                            for msg in glaive_conversation:
-                                role, content = msg["from"], msg["value"]
-                                if role == "system":
-                                    system_msg = clean_message(content)
-                                if role == "human":
-                                    user_msg = clean_message(content)
-                                if role == "gpt":
-                                    accepted_msg = clean_message(content)
-                                    rejected_msg = completion
-                                    break
-                            new_rows_batch.append(
-                                {
-                                    "system": system_msg,
-                                    "question": user_msg,
-                                    "chosen": accepted_msg,
-                                    "rejected": rejected_msg,
-                                }
-                            )
-
-                        print_result_dicts(new_rows_batch)
-                        new_dataset_rows.extend(new_rows_batch)
-
-            if i % upload_every == 0 and i > 0:
-                upload_dataset(
-                    output_dataset, config.output_dataset_name, new_dataset_rows
-                )
-
 
 if __name__ == "__main__":
     fire.Fire(main)
--- synthetic_data/tasks.py
+++ synthetic_data/tasks.py
@@ -1,15 +1,29 @@
 from abc import ABC
 from enum import Enum
+import random
 from typing import Dict, List, Optional
+from synthetic_data.prompts import (
+    TOOL_USE_CATEGORIES,
+    format_dalle_prompt_template,
+    get_toolformer_dpo_negative_completion_prompt,
+    get_toolformer_prompt,
+)
 
-from synthetic_data.utils import DatasetTaskFormat, SeedDataFormat
+from synthetic_data.utils import (
+    Conversation,
+    DatasetTaskFormat,
+    SeedDataFormat,
+    ToolFormerRow,
+    assert_valid_python_code,
+    extract_tool_usage_dpo_row,
+)
 
 
 class SyntheticDataTask(ABC):
 
     seed_data_format: SeedDataFormat
     dataset_task_format: DatasetTaskFormat = DatasetTaskFormat.SFT
-    
+
     # Only used for the toolformer DPO dataset. # TODO remove this when that project is finished.
     seed_data_uses_conversation_format: bool = False
 
@@ -23,15 +37,15 @@ class SyntheticDataTask(ABC):
 
     empty_dataset_format: Dict[str, List]
 
-    def prompt_template(self) -> str:
+    def get_seed_input_conversation(self, batch: Dict) -> List[Conversation]:
         """
-        Prompt template to use for generating data.
+        Prompt template to use for generating initial seed data.
         """
         raise NotImplementedError
 
-    def validation_function(self, dataset_row: Dict) -> bool:
+    def post_generation_format_row(self, completion: str) -> Dict:
         """
-        Return whether a dataset row is valid.
+        Take the completed conversation and format it into the final dataset format.
         """
         return True
 
@@ -55,8 +69,15 @@ class PromptUpsample(SyntheticDataTask):
         "Upsampled": [],
     }
 
-    def prompt_template(self) -> str:
-        return "Upsample the following prompt:"
+    def __init__(self) -> None:
+        super().__init__()
+        self.original_rows = []
+
+    def get_seed_input_conversation(self, batch: Dict) -> List[Conversation]:
+        prompts, categories_batch = batch["Prompt"], batch["Category"]
+        return [
+            format_dalle_prompt_template(prompt) for prompt in prompts
+        ]
 
     def validation_function(self, dataset_row: Dict) -> bool:
         return len(dataset_row["upsampled_prompt"]) > 0
@@ -78,3 +99,96 @@ class Toolformer(SyntheticDataTask):
         "chosen": [],
         "rejected": [],
     }
+
+    def get_seed_input_conversation(self, batch_size: int) -> List[Conversation]:
+        prompt_conversations: List[Conversation] = []
+        random_categories = random.sample(TOOL_USE_CATEGORIES * batch_size, batch_size)
+        for category in random_categories:
+            prompt_conversations.append(get_toolformer_prompt(category))
+        return prompt_conversations
+
+    def get_seed_input_conversation(self, batch: Dict) -> List[Conversation]:
+        # TODO chance of dropping out tool definition
+        conversations_batch = []
+        for conversation in batch["conversations"]:
+            messages = [message["content"] for message in conversation]
+            question, tool_call, call_result, agent_output = messages
+            original_row = ToolFormerRow(
+                question=question,
+                call_result=call_result,
+                tool_call=tool_call,
+                agent_output=agent_output,
+            )
+            conversation = get_toolformer_dpo_negative_completion_prompt(question)
+            conversations_batch.append(conversation)
+        return conversations_batch
+
+    def post_generation_format_row(self, completion: str) -> Dict:
+        row = extract_tool_usage_dpo_row(completion)
+
+        assert_valid_python_code(row.definition)
+        assert_valid_python_code(row.call_result)
+        assert_valid_python_code(row.tool_call)
+
+        return {
+            "tool": row.definition,
+            "question": row.task,
+            "call_result": row.call_result,
+            "tool_call": row.tool_call,
+            "agent_output": row.agent_output,
+        }
+
+
+class GlaiveDPO(SyntheticDataTask):
+
+    def get_seed_input_conversation(self, batch: Dict) -> List[Conversation]:
+        glaive_conversations = [chatml_to_conversation(chat, system) for chat, system in zip(batch["chat"], batch["system"])]  # type: ignore
+        prompt_conversations: List[Conversation] = []
+        for conversation in glaive_conversations:
+            completion_conv = []
+            for msg in conversation:
+                if msg["from"] == "gpt":
+                    break
+                completion_conv.append(
+                    {
+                        "role": SHAREGPT_TO_OPENAI_ROLE[msg["from"]],
+                        "content": msg["value"],
+                    }
+                )
+            prompt_conversations.append(completion_conv)
+        return prompt_conversations
+
+    def _clean_glaive_conversation(self, conversation: List[Dict]) -> List[Dict]:
+        for completion, glaive_conversation in zip(
+            completions, glaive_conversations
+        ):
+
+            system_msg, user_msg, accepted_msg, rejected_msg = (
+                "",
+                "",
+                "",
+                "",
+            )
+            for msg in glaive_conversation:
+                role, content = msg["from"], msg["value"]
+                if role == "system":
+                    system_msg = clean_message(content)
+                if role == "human":
+                    user_msg = clean_message(content)
+                if role == "gpt":
+                    accepted_msg = clean_message(content)
+                    rejected_msg = completion
+                    break
+            new_rows_batch.append(
+                {
+                    "system": system_msg,
+                    "question": user_msg,
+                    "chosen": accepted_msg,
+                    "rejected": rejected_msg,
+                }
+            )
+
+        return [
+            {"role": SHAREGPT_TO_OPENAI_ROLE[msg["from"]], "content": msg["value"]}
+            for msg in conversation
+        ]
