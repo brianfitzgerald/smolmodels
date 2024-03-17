2f3ef880431f7a87e2176d2e1bdf7ff07f36d95f
brianf <brian@stability.ai>
2024-03-17 02:02:16 +0000

    parquet support

--- evaluate_seq2seq.py
+++ evaluate_seq2seq.py
@@ -26,7 +26,7 @@ def format_filename(string: str, limit: int = 20):
 
 def main(
     checkpoint_file: str = "checkpoints/prompt_safety-zCN6",
-    samples_file: Optional[str] = None,
+    dataset_file: Optional[str] = None,
     config: str = "prompt_safety",
     batch_size: int = 8,
     upload_to_hf: bool = False,
@@ -84,18 +84,22 @@ def main(
     out_dir = f"samples/{model_config.ckpt_name}"
     ensure_directory(out_dir, clear=True)
 
-    if samples_file:
-        print(f"Reading samples from: {samples_file}")
-        validation_df: pd.DataFrame = pd.read_csv(samples_file)
+    if dataset_file:
+        print(f"Reading samples from: {dataset_file}")
+        if dataset_file.endswith(".csv"):
+            validation_df: pd.DataFrame = pd.read_csv(dataset_file)
+        elif dataset_file.endswith(".parquet"):
+            validation_df: pd.DataFrame = pd.read_parquet(dataset_file)
+            validation_df = validation_df[(validation_df['nsfw_regex'] == True) | (validation_df['nsfw_image'] == True)]
         for i in range(0, len(validation_df), batch_size):
 
             chunk = validation_df[i : i + batch_size]
 
             prompts_with_prefix = [
-                model_config.task_prefix + sentence for sentence in chunk["Prompt"]
+                model_config.task_prefix + sentence for sentence in chunk["prompt"]
             ]
 
-            inputs = tokenizer(prompts_with_prefix, return_tensors="pt", padding=True)
+            inputs = tokenizer(prompts_with_prefix, return_tensors="pt", padding=True).to("cuda")
 
             output_sequences = model.generate(
                 input_ids=inputs["input_ids"],
@@ -105,11 +109,11 @@ def main(
             )
 
             out = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
-            for j, (prompt, upsampled) in enumerate(zip(chunk["Prompt"], out)):
-                print(f"Prompt: {prompt}\nUpsampled: {upsampled}\n\n")
+            for j, (prompt, generated) in enumerate(zip(chunk["prompt"], out)):
+                print(f"Prompt: {prompt}\nGenerated: {generated}\n\n")
 
                 if generate_samples:
-                    for k, txt in enumerate([prompt, upsampled]):
+                    for k, txt in enumerate([prompt, generated]):
                         print(f"Generating sample for: {txt}")
                         image: Image.Image = pipe(txt, num_inference_steps=30, guidance_scale=20).images[0]  # type: ignore
                         prompt_fmt = format_filename(txt)
@@ -137,8 +141,8 @@ def main(
                 labels, skip_special_tokens=True
             )
             print(f"Prompts: {prompt_strings}\nSanitized: {out}\n\n")
-            for prompt, upsampled in zip(prompt_strings, out):
-                rows.append({"Prompt": prompt, "Upsampled": upsampled})
+            for prompt, generated in zip(prompt_strings, out):
+                rows.append({"Prompt": prompt, "Upsampled": generated})
         
             if i % 100 == 0:
                 print(f"Processed {i} samples, saving...")
