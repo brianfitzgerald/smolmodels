661f9e68df9236ab883df11874011f4643b9c59b
brianf <brian@stability.ai>
2024-04-18 02:32:57 +0000

    add submit script

---
+++ submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=sm
+#SBATCH --output=sm.out.%j
+#SBATCH --error=sm.out.%j
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --account=engineering
+#SBATCH --partition=h80i
+#SBATCH --priority=normal
+
+
+python train.py --wandb
