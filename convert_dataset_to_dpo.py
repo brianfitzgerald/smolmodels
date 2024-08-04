9b5d1676cb0d2d5c45e676298a5de0438e325b72
brianf <brian@stability.ai>
2024-08-04 00:55:56 +0000

    dpo merge script

---
+++ convert_dataset_to_dpo.py
@@ -0,0 +1,31 @@
+from datasets import load_dataset, Dataset
+from tqdm import tqdm
+
+alpaca = load_dataset("yahma/alpaca-cleaned", split="train")
+goody = load_dataset("roborovski/open-goody2", split="train")
+
+
+original_outputs = {item["instruction"]: item["output"] for item in alpaca}
+
+merged_data = []
+for sample in tqdm(goody):
+    instruction = sample["instruction"]
+    if instruction in original_outputs:
+        merged_item = {
+            "chosen": sample["response"],
+            "instruction": instruction,
+            "rejected": original_outputs[instruction],
+        }
+        print(merged_item)
+        merged_data.append(merged_item)
+
+
+merged_dataset = Dataset.from_dict(
+    {
+        "chosen": [item["chosen"] for item in merged_data],
+        "instruction": [item["instruction"] for item in merged_data],
+        "rejected": [item["rejected"] for item in merged_data],
+    }
+)
+
+merged_dataset.push_to_hub("roborovski/open-goody2-dpo")
