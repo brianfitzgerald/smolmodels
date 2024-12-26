0f4a923fac731ec35388aee478189f79f95e7173
brianf <brian@stability.ai>
2024-12-26 04:45:14 +0000

    wip

--- scripts/run_vllm_server.sh
+++ scripts/run_vllm_server.sh
@@ -1,4 +1,5 @@
 export VLLM_ALLOW_RUNTIME_LORA_UPDATING=True
+export HF_HOME="/weka/home-brianf/huggingface"
 
 set -ex
 
@@ -8,4 +9,5 @@ BASE_MODEL=$(jq -r '.base_model_name_or_path' "$CONFIG_PATH")
 
 echo "Starting VLLM server with model: $BASE_MODEL and LoRA directory: $LORA_DIR"
 
-vllm serve $BASE_MODEL --enable-lora --lora-modules dpo_lora=$LORA_DIR/
+vllm serve $BASE_MODEL --enable-lora --lora-modules dpo_lora=$LORA_DIR/ --tokenizer_mode "mistral"
+# vllm serve $BASE_MODEL --tokenizer_mode "mistral"
