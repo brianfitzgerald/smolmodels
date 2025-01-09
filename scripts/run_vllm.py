a942da7970d5a9810c72bf015246f9bd492b619f
brianf <brian@stability.ai>
2025-01-09 16:08:37 +0000

    wip

--- scripts/run_vllm.py
+++ scripts/run_vllm.py
@@ -21,11 +21,17 @@ prometheus_multiproc_dir: tempfile.TemporaryDirectory
 logger = init_logger("vllm.entrypoints.openai.api_server")
 
 
-def main(run: str = "", steps: Optional[int] = None):
+def main(
+    model: Optional[str] = None, run: Optional[str] = None, steps: Optional[int] = None
+):
     """
-    Load the run specified by the run_name.
+    If model is provided, use that ckpt.
+    If run is provided, use the run specified by the run_name.
     If steps is provided, use that ckpt, otherwise use latest.
     """
+
+    if model and run:
+        raise ValueError("Only one of model or run should be provided")
 
     parser = FlexibleArgumentParser(
         description="vLLM OpenAI-Compatible RESTful API server."
@@ -34,23 +40,26 @@ def main(run: str = "", steps: Optional[int] = None):
     # don't actually parse cli args, just return the object
     args = parser.parse_args([])
 
-    run_directory = f"/weka/home-brianf/runs/{run}"
-    if not os.path.exists(run_directory):
-        raise ValueError(f"Run directory {run_directory} not found")
-    checkpoints = os.listdir(run_directory)
-    checkpoints = [x for x in checkpoints if x.startswith("checkpoint-")]
-    sorted_checkpoints = list(sorted(checkpoints, key=lambda x: int(x.split("-")[-1])))
-    sorted_checkpoints = [int(x.split("-")[-1]) for x in sorted_checkpoints]
-    if steps in sorted_checkpoints:
-        checkpoint_dir = f"{run_directory}/checkpoint-{steps}"
-    elif steps is not None and steps not in sorted_checkpoints:
-        raise ValueError(f"Checkpoint {steps} not found in {run_directory}")
-    else:
-        latest_ckpt = sorted_checkpoints[-1]
-        checkpoint_dir = f"{run_directory}/checkpoint-{latest_ckpt}"
+    if run:
+        run_directory = f"/weka/home-brianf/runs/{run}"
+        if not os.path.exists(run_directory):
+            raise ValueError(f"Run directory {run_directory} not found")
+        checkpoints = os.listdir(run_directory)
+        checkpoints = [x for x in checkpoints if x.startswith("checkpoint-")]
+        sorted_checkpoints = list(sorted(checkpoints, key=lambda x: int(x.split("-")[-1])))
+        sorted_checkpoints = [int(x.split("-")[-1]) for x in sorted_checkpoints]
+        if steps in sorted_checkpoints:
+            checkpoint_dir = f"{run_directory}/checkpoint-{steps}"
+        elif steps is not None and steps not in sorted_checkpoints:
+            raise ValueError(f"Checkpoint {steps} not found in {run_directory}")
+        else:
+            latest_ckpt = sorted_checkpoints[-1]
+            checkpoint_dir = f"{run_directory}/checkpoint-{latest_ckpt}"
 
-    logger.info(f"Using checkpoint: {checkpoint_dir}")
-    args.model = checkpoint_dir
+        logger.info(f"Using checkpoint: {checkpoint_dir}")
+        args.model = checkpoint_dir
+    elif model:
+        args.model = model
     validate_parsed_serve_args(args)
 
     uvloop.run(run_server(args))
